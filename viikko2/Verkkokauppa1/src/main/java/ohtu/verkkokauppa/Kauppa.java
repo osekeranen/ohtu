ac447110ffdfb783e3b7b1067beb721188cc14bb
Oskari Keränen <oskari.e.keranen@gmail.com>
2020-11-10 00:05:02 +0200

    Add Spring

--- viikko2/Verkkokauppa1/src/main/java/ohtu/verkkokauppa/Kauppa.java
+++ viikko2/Verkkokauppa1/src/main/java/ohtu/verkkokauppa/Kauppa.java
@@ -1,5 +1,9 @@
 package ohtu.verkkokauppa;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 public class Kauppa {
 
     private VarastoOlio varasto;
@@ -8,6 +12,7 @@ public class Kauppa {
     private ViitegeneraattoriOlio viitegeneraattori;
     private String kaupanTili;
 
+    @Autowired
     public Kauppa(VarastoOlio varasto, PankkiOlio pankki, ViitegeneraattoriOlio viitegeneraattori) {
         this.varasto = varasto;
         this.pankki = pankki;
--- viikko2/Verkkokauppa1/src/main/java/ohtu/verkkokauppa/Varasto.java
+++ viikko2/Verkkokauppa1/src/main/java/ohtu/verkkokauppa/Varasto.java
@@ -1,12 +1,17 @@
 package ohtu.verkkokauppa;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
 import java.util.*;
 
+@Component
 public class Varasto implements VarastoOlio {
 
     private Kirjanpito kirjanpito;
     private HashMap<Tuote, Integer> saldot;  
-    
+
+    @Autowired
     public Varasto(Kirjanpito kirjanpito) {
         this.kirjanpito = kirjanpito;
         saldot = new HashMap<Tuote, Integer>();
